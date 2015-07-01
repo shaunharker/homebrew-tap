4a65b0c0884b54b73eebf817aebe6fad5b288ba5
Shaun Harker <sharker81@gmail.com>
2015-07-01 16:25:02 -0400

    fixed sha code

--- dsgrn.rb
+++ dsgrn.rb
@@ -3,7 +3,7 @@ class Dsgrn < Formula
   homepage "https://github.com/shaunharker/DSGRN"
   url "https://github.com/shaunharker/DSGRN/archive/0.1.tar.gz"
   version "0.1"
-  sha256 "4af31ef3cbe74fe372ad75244178ff931a6bafbd9c26a79cbf69742860a564dc"
+  sha256 "2fce6fa4b9713c9d897c21892432689f3c32b091d4b2038e68c5e8e68b869744"
 
   depends_on "cmake" => :build
   depends_on "shaunharker/tap/cluster-delegator" => :build  
