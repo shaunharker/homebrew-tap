bdda82568dfb74d394a211e334dd70a051093a8e
Shaun Harker <sharker81@gmail.com>
2015-07-01 13:41:05 -0400

    added cluster-delegator formula

---
+++ cluster-delegator.rb
@@ -0,0 +1,15 @@
+class ClusterDelegator < Formula
+  desc "cluster-delegator"
+  homepage "https://github.com/shaunharker/cluster-delegator"
+  url "https://github.com/shaunharker/cluster-delegator/archive/v1.1.tar.gz"
+  version "1.1"
+  sha256 "21697f52630709dc448e776d2ded96216fd1a6c03ea1fb4ff90982a261f5fa14"
+
+  def install
+    system "./install.sh"
+  end
+
+  test do
+    system "true"
+  end
+end
