4b503dd3e14c2291d5eaac477a71e5c8ac23ed00
Shaun Harker <sharker81@gmail.com>
2015-07-05 04:46:12 -0400

    updating cluster-delegator brew to v2.0

--- cluster-delegator11.rb
+++ cluster-delegator11.rb
@@ -1,8 +1,8 @@
 class ClusterDelegator < Formula
   desc "cluster-delegator"
   homepage "https://github.com/shaunharker/cluster-delegator"
-  url "https://github.com/shaunharker/cluster-delegator/archive/v2.0.tar.gz"
-  version "2.0"
+  url "https://github.com/shaunharker/cluster-delegator/archive/v1.1.tar.gz"
+  version "1.1"
   sha256 "21697f52630709dc448e776d2ded96216fd1a6c03ea1fb4ff90982a261f5fa14"
 
   depends_on "boost" => :build
