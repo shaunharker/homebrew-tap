c04ec0a63026166de2e4ab61262f27b6726dcfd2
Shaun Harker <sharker81@gmail.com>
2015-07-05 04:48:35 -0400

    fixed sha256 code on cluster-delegator

--- cluster-delegator.rb
+++ cluster-delegator.rb
@@ -3,7 +3,7 @@ class ClusterDelegator < Formula
   homepage "https://github.com/shaunharker/cluster-delegator"
   url "https://github.com/shaunharker/cluster-delegator/archive/v2.0.tar.gz"
   version "2.0"
-  sha256 "21697f52630709dc448e776d2ded96216fd1a6c03ea1fb4ff90982a261f5fa14"
+  sha256 "5bfe84f7739f7c80f569c42e60cdc35f2f91970a9a1b8415fbe4224543e37d77"
 
   depends_on "boost" => :build
 
